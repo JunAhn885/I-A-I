716ebb53771733d2f5b86503f694cc057d6f85ce
AllenLiu12138 <xliu28@uw.edu>
2023-04-15 12:49:06 -0700

    add family log

---
+++ xuliu7/src/Components/FamilyLog/EditLog.js
@@ -0,0 +1,36 @@
+import React from "react";
+import classes from "./EditLog.module.css";
+import edit from "./../../img/edit.svg";
+import deleteIcon from "./../../img/Delete.svg";
+import attach from "./../../img/attachFile.svg";
+
+const EditLog = () => {
+  return (
+    <div className={classes.editLogTop}>
+      <div className={classes.pageName}>
+        <h1>Family Log</h1>
+      </div>
+      <div className={classes.editMainContent}>
+        <div className={classes.headerMain}>
+          <h1>First photo shoot</h1>
+          <img src={edit} alt="edit" />
+          <img src={deleteIcon} alt="deleteIcon" />
+        </div>
+        <p className={classes.dateName}>03/01/2023 BeanKeeper Studio</p>
+
+        <p className={classes.editMessage}>
+          Hi, [user name], how did this event go? Record your experience
+        </p>
+        <div className={classes.messagearea}>
+          <div className={classes.textarea}>
+            <textarea className={classes.message}></textarea>
+            <img src={attach} alt="attach" /> 
+          </div>
+          <button>update</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EditLog;
---
+++ xuliu7/src/Components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import classes from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <div className={classes.headerTop}>
+      <ul>
+        <li>Dashboard</li>
+        <li>Tasks</li>
+        <li>Bonding Journal</li>
+        <NavLink to="family-log">
+          <li>Family Log</li>
+        </NavLink>
+        <li>Profile</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
