e86b6ee25d19ffa628d3bf40f6031e840b522798
Jun Ahn <junahn@Juns-MacBook-Air-4.local>
2023-05-22 17:34:46 -0700

    about us ection done

--- frontend/src/components/landingpage/aboutUs.js
+++ frontend/src/components/landingpage/aboutUs.js
@@ -1,7 +1,9 @@
-const AboutUs = () => {
+const AboutUs = (props) => {
     return (
-        <div className="about-us">
-            
+        <div className="member">
+            <img src={props.image} alt={props.name}></img>
+            <h2 className="member-name">{props.name}</h2>
+            <p classNmae="member role">{props.role}</p>
         </div>
     )
 }
