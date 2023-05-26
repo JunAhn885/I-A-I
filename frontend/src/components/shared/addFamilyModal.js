3da1d8f7bfb13f3ba01382f3d4cf07b6f73ca8a3
Jun Ahn <junahn@Juns-MacBook-Air-4.local>
2023-05-25 21:27:20 -0700

    add family error message

--- frontend/src/components/shared/addFamilyModal.js
+++ frontend/src/components/shared/addFamilyModal.js
@@ -10,21 +10,19 @@ const AddFamilyModal = (props) => {
 
     const addUserToggle = async() => {
         let user = JSON.parse(localStorage.getItem('user'));
-        console.log('User:', user);
         try {
             const response = await UserService.updateFamily(user.id, userName);
-            console.log('Response:', response);
-            if (response.status === 400) {
-              alert("Cannot find user. Please enter a valid username");
-            } else if (response.status === 500) {
-              alert("Error: " + response.data.message);
-            } else if (response.status === 401) {
-              alert("You must be logged in to add a user to your family");
-            } else {
-              alert("User added to the family");
-            }
           } catch (error) {
-            console.error("An error occurred:", error);
+            let response = error.response
+            if (response.status === 400) {
+                alert("Cannot find user. Please enter a valid username");
+              } else if (response.status === 500) {
+                alert("Error: " + response.data.message);
+              } else if (response.status === 401) {
+                alert("You must be logged in to add a user to your family");
+              } else {
+                alert("User added to the family");
+              }
         }
     }
 
