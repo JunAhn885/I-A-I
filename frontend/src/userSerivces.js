dc7ea03943ecbc9e824c85099d469404b171b9d4
Jun Ahn <junahn@Juns-MacBook-Air-4.local>
2023-05-26 09:54:25 -0700

    serverurl

--- frontend/src/userSerivces.js
+++ frontend/src/userSerivces.js
@@ -2,7 +2,7 @@ import { typeImplementation } from '@testing-library/user-event/dist/type/typeIm
 import axios from 'axios';
 
 const serverUrl = 'https://squid-app-hq6q4.ondigitalocean.app/';
-
+  
 // posts for bonding journal 
 const getBJPosts = (id, year, month, day) => axios.get(`${serverUrl}/posts?id=${id}&year=${year}&month=${month}&day=${day}`);
 
